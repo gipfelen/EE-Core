28c06351596c3ca3488d3269fca5c9054c877dc3
Fedor Smirnov <fedor@dps.uibk.ac.at>
2020-12-12 10:37:17 +0100

    setting up the CI

---
+++ src/test/java/at/uibk/dps/ee/core/EeCoreAbstractTest.java
@@ -0,0 +1,25 @@
+package at.uibk.dps.ee.core;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.Test;
+
+import at.uibk.dps.ee.core.enactable.EnactmentStateListener;
+
+public class EeCoreAbstractTest {
+
+	protected class EeCoreMock extends EeCoreAbstract{
+		public EeCoreMock(InputDataProvider inputDataProvider, OutputDataHandler outputDataHandler,
+				WorkflowProvider applicationProvider, Set<EnactmentStateListener> stateListeners) {
+			super(inputDataProvider, outputDataHandler, applicationProvider, stateListeners);
+		}
+	}
+	
+	@Test
+	public void test() {
+		assertEquals(3, 2 + 1);
+	}
+
+}
