793c8369cc3ba0d0b6340c118c73f1ab4896067c
Fedor Smirnov <fedor@dps.uibk.ac.at>
2021-02-13 10:00:36 +0100

    stop exception extended

--- src/main/java/at/uibk/dps/ee/core/exception/StopException.java
+++ src/main/java/at/uibk/dps/ee/core/exception/StopException.java
@@ -1,5 +1,6 @@
 package at.uibk.dps.ee.core.exception;
 
+import java.util.Optional;
 import at.uibk.dps.ee.core.enactable.Enactable;
 
 /**
@@ -11,11 +12,10 @@ import at.uibk.dps.ee.core.enactable.Enactable;
  */
 public class StopException extends Exception {
 
-  /**
-   * 
-   */
   private static final long serialVersionUID = 1L;
 
+  protected String stopMessage;
+  protected Optional<Exception> initialException = Optional.empty();
 
   /**
    * Constructed by providing the reason for the stop.
@@ -23,6 +23,51 @@ public class StopException extends Exception {
    * @param stoppingReason the reason for the stop
    */
   public StopException(final String message) {
-    super(message);
+    this.stopMessage = message;
+  }
+
+  /**
+   * Constructor where the initial exception shall be stored in the
+   * {@link StopException}.
+   * 
+   * @param message the infor message
+   * @param exception the initial exception
+   */
+  public StopException(final String message, final Exception exception) {
+    this.stopMessage = message;
+    this.initialException = Optional.of(exception);
+  }
+
+  /**
+   * Returns true if this stopexception contains an initial exception.
+   * 
+   * @return true if this stopexception contains an initial exception
+   */
+  public boolean hasInitialException() {
+    return initialException.isPresent();
+  }
+
+  /**
+   * Appends additional information onto the current stop message.
+   * 
+   * @param additionalInformation the additional information.
+   */
+  public void appendExceptionInformation(final String additionalInformation) {
+    stopMessage += "\n";
+    stopMessage += additionalInformation;
+  }
+
+  public String getStopMessage() {
+    return stopMessage;
+  }
+
+  /**
+   * Returns the initial exception--should be used AFTER checking that there
+   * actually is an initial exception by calling hasInitialException().
+   * 
+   * @return the initial exception.
+   */
+  public Exception getInitialException() {
+    return initialException.get();
   }
 }
--- src/test/java/at/uibk/dps/ee/core/exception/StopExceptionTest.java
+++ src/test/java/at/uibk/dps/ee/core/exception/StopExceptionTest.java
@@ -1,7 +1,7 @@
 package at.uibk.dps.ee.core.exception;
 
 import static org.junit.Assert.*;
-
+import java.util.NoSuchElementException;
 import org.junit.Test;
 
 public class StopExceptionTest {
@@ -10,7 +10,28 @@ public class StopExceptionTest {
   public void testGetMessage() {
     String message = "message";
     StopException tested = new StopException(message);
-    assertEquals(message, tested.getMessage());
+    assertEquals(message, tested.getStopMessage());
+    String additionalInfo = "additional";
+    String expected = message + "\n" + additionalInfo;
+    tested.appendExceptionInformation(additionalInfo);
+    assertEquals(expected, tested.getStopMessage());
+    assertFalse(tested.hasInitialException());
   }
 
+  @Test
+  public void testGetMessageExc() {
+    String message = "message";
+    IllegalArgumentException exc = new IllegalArgumentException("whatever");
+    StopException tested = new StopException(message, exc);
+    assertEquals(message, tested.getStopMessage());
+    assertTrue(tested.hasInitialException());
+    assertEquals(exc, tested.getInitialException());
+  }
+
+  @Test(expected = NoSuchElementException.class)
+  public void testGetMessageExcNotSet() {
+    String message = "message";
+    StopException tested = new StopException(message);
+    tested.getInitialException();
+  }
 }
